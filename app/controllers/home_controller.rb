b3fcc131bc1f335cd82a4eaf74bacb6f1f7e58b7
macournoyer <macournoyer@gmail.com>
2013-03-27 12:28:32 -0400

    Implement before_filter.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_filter :header
+
   def index
-    response.write "Hello from home controller"
+    response.write "<p>Hello from home controller</p>"
+  end
+
+  def header
+    response.write "<h1>My App</h1>"
   end
 end
