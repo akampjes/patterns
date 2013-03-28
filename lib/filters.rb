c174ffb3bb4d3f40e0282923adf1cf96e7b332c1
macournoyer <macournoyer@gmail.com>
2013-03-28 10:09:22 -0400

    Use concern and callbacks from AS.

--- lib/filters.rb
+++ lib/filters.rb
@@ -1,60 +1,31 @@
+require "active_support/concern"
+require "active_support/callbacks"
+
 module Filters
-  def self.included(base)
-    # base == ActionController::Base
-    base.extend ClassMethods
+  extend ActiveSupport::Concern
+  include ActiveSupport::Callbacks
+
+  included do
+    define_callbacks :process
   end
 
   module ClassMethods
     def before_filter(method)
-      around_filter do |controller, action|
-        controller.send method
-        action.call
-      end
+      set_callback :process, :before, method
     end
 
     def after_filter(method)
-      around_filter do |controller, action|
-        action.call
-        controller.send method
-      end
+      set_callback :process, :after, method
     end
 
-    def around_filters
-      @around_filters ||= []
-    end
-
-    # around_filter :layout
-    # def layout
-    #   response.write "<html>"
-    #   yield
-    #   response.write "</html>"
-    # end
-    # around_filter { |controller, action| action.call }
     def around_filter(method=nil, &block)
-      if block
-        around_filters << block
-      else
-        around_filters << proc { |controller, action| controller.send method, &action }
-      end
+      set_callback :process, :around, method, &block
     end
   end
 
   def process(action)
-    # around_filter :one
-    # around_filter :two
-    # 
-    # proc do |controller, action|
-    #   # run around one logic
-    #   action.call
-    # end.call(controller, action1)
-    # action1 = proc do |controller, action|
-    #   # run around two logic
-    #   action.call
-    # end.call(controller, action2)
-    # action2 = proc { super }
-
-    self.class.around_filters.reverse.inject(proc { super }) do |parent_proc, filter|
-      proc { filter.call(self, parent_proc) }
-    end.call
+    run_callbacks :process do
+      super
+    end
   end
 end
